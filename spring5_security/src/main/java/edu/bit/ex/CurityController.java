c8640eb24dc3ceb90b547c13808253c17caa410c
castle1 <dlf5401@naver.com>
2021-02-18 18:41:50 +0900

    DB 얀동 EMP

--- spring5_security/src/main/java/edu/bit/ex/CurityController.java
+++ spring5_security/src/main/java/edu/bit/ex/CurityController.java
@@ -1,19 +1,10 @@
 package edu.bit.ex;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import jdk.internal.org.jline.utils.Log;
 import lombok.extern.log4j.Log4j;
 
 /**
@@ -27,17 +18,17 @@ public class CurityController {
 	
 	@GetMapping("/all")
 	public void doAall() {
-		//Log.info("do all can access everybody");
+		log.info("do all can access everybody");
 	}
 	
 	@GetMapping("/member")
 	public void domember() {
-		//Log.info("do all can access member");
+		log.info("do all can access member");
 	}
 	
 	@GetMapping("/admin")
 	public void doadmin() {
-		//Log.info("do all can access member");
+		log.info("do all can access member");
 	}
 	
     @GetMapping("/accessError")
